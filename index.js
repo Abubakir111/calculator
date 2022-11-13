7eab6ecd3511befa2493e1618fb6cdc2488eb628
bakir <bakirbazarkul16@gmail.com>
2022-11-13 19:44:50 +0300

    remove js

--- index.js
+++ index.js
@@ -1,27 +1,53 @@
 let bloc_btn = document.querySelector(".btnContent");
 let result = document.querySelector(".val");
 let his = document.querySelector(".history");
+let histroylist = document.querySelector(".histroylist");
+let btn_history = document.querySelector(".btn_history ");
+let h1 = document.querySelector(".h1");
+let btn_back = document.querySelector(".btnback");
+let display = document.querySelector(".display");
 let value;
-let test;
-
+let history = "";
 bloc_btn.addEventListener("click",(event)=>{
-value = event.target.innerText;
-    switch(value){
-      case "c":
-      result.innerText =  result.innerText.substring(0,result.innerText.length-1);
-      result.innerText = result.innerText;
-        break;
-        case "ac":
-          result.innerText = "";
-          break;
-      case "=":
-        his.innerText = result.innerText ;
-        result.innerText = eval(result.innerText);
-        break;
-     
-        default:
-          result.innerText +=value;
+  if(!event.target.classList.contains("btn")) return;
+      value = event.target.innerText;
+
+      switch(value){
+         case "C":
+            result.innerText =  result.innerText.substring(0,result.innerText.length-1);
+            result.innerText = result.innerText;
+            break;
+         case "AC":
+            result.innerText = "";
+            break;
+         case "=":
+            his.innerText = result.innerText ;
+            result.innerText = eval(result.innerText);
+            history  += `
+               <li>${his.innerText}<br>
+                     <span>${result.innerText}</span>
+               </li>`
+               histroylist.innerHTML  = history;      
+               break;
+         default:
+            result.innerText +=value;
 
     }
-    
-});
+
+  });
+  btn_history.addEventListener("click",()=>{
+        bloc_btn.style.display = "none";
+        display.style.display = "none";
+        histroylist.style.display = "block";
+        h1.style.display =  "block";
+        btn_back.style.display = "block";
+     
+  });
+  btn_back.addEventListener("click",()=>{
+   bloc_btn.style.display = "block";
+   display.style.display = "block";
+   histroylist.style.display = "none";
+   h1.style.display =  "none";
+   btn_back.style.display = "none";
+  });
+
