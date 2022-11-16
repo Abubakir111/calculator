6b504ac006db26d156dbd9f9b645d732480a0880
bakir <bakirbazarkul16@gmail.com>
2022-11-16 18:57:10 +0300

    wrapping it in a function

--- chrom_calc/index.js
+++ chrom_calc/index.js
@@ -1,6 +1,6 @@
   
   
-  function alerttik(){
+ function browser_version(){
 
 let bloc_btn = document.querySelector(".btnContent");
 let result = document.querySelector(".val");
@@ -12,89 +12,65 @@ let btn_back = document.querySelector(".btnback");
 let display = document.querySelector(".display");
 let value;
 let history = "";
-        // alert("функция 1");
-        console.log(window.innerWidth);
 
         bloc_btn.addEventListener("click",(event)=>{
             if(!event.target.classList.contains("btn")) return;
-                value = event.target.innerText;
+            value = event.target.innerText;
+        
+            switch(value){
+                case "C":
+                    result.innerText =  result.innerText.substring(0,result.innerText.length-1);
+                    result.innerText = result.innerText;
                 
-                switch(value){
-                   case "C":
-                      result.innerText =  result.innerText.substring(0,result.innerText.length-1);
-                      result.innerText = result.innerText;
-                    
-                      if(result.innerText == "" && !his.innerText == ""){
-                         result.innerText = his.innerText;
-                         his.innerText = "";
-                      }
-                      break;
-                   case "=":
-                      his.style.color = "rgb(143, 139, 139)"
-                      his.innerText = result.innerText ;
-                      result.innerText = eval(result.innerText);
-                      history  += `
-                         <li class="li">${his.innerText}<br>
-                               <span>${result.innerText}</span>
-                         </li>`
-                         histroylist.innerHTML  = history;   
-                         if(his.innerText.length > 12){
-                          his.style.fontSize = "0.9em";
-                         }   
-                         break;
-                   default:
-                      result.innerText +=value;
-          
-              }
-                 if(result.innerText.length == 11){
-                    result.style. fontSize =  "1.7em";
-                    result.style.lineHeight = "45px";
-                 }
-                 if(result.innerText.length == 12){
-                   result.style. fontSize =  "1.6em";
-                   result.style.lineHeight = "45px";
-                  }
-                  if(result.innerText.length == 13){
-                   result.style. fontSize =  "1.5em";
-                   result.style.lineHeight = "45px";
-                  }
-                  if(result.innerText.length == 14){
-                   result.style. fontSize =  "1.4em";
-                   result.style.lineHeight = "45px";
-                  }
-                  if(result.innerText.length == 15){
-                   result.style. fontSize =  "1.3em";
-                   result.style.lineHeight = "45px";
-                  }
-                  if(result.innerText.length == 16){
-                   result.style. fontSize =  "1.2em";
-                   result.style.lineHeight = "45px";
-                  }
-                 
-                  
-                   if(result.innerText.length < 10){
-                   display.style.color = "#fff";
-                   result.style. fontSize =  "1.8em";
-                  }
-               
-            });
-            btn_history.addEventListener("click",()=>{
-                  bloc_btn.style.display = "none";
-                  display.style.display = "none";
-                  histroylist.style.display = "block";
-                  h1.style.display =  "block";
-                  btn_back.style.display = "block";
-               
-            });
-            btn_back.addEventListener("click",()=>{
-                   bloc_btn.style.display = "block";
-                   display.style.display = "block";
-                   histroylist.style.display = "none";
-                   h1.style.display =  "none";
-                   btn_back.style.display = "none";
-            });
-           }
-  function wit(){
-         alert("функция 2");
-            } 
-export{alerttik , wit};
+                    if(result.innerText == "" && !his.innerText == ""){
+
+                            result.innerText = his.innerText;
+                            his.innerText = "";
+
+                        }
+                    break;
+                case "=":
+                    his.style.color = "rgb(143, 139, 139)"
+                    his.innerText = result.innerText ;
+                    result.innerText = eval(result.innerText);
+                    history  += `
+                        <li class="li">${his.innerText}<br>
+                            <span>${result.innerText}</span>
+                        </li>`
+                    histroylist.innerHTML  = history;   
+                    if(his.innerText.length > 12) his.style.fontSize = "0.9em";
+                    break;
+                default: result.innerText +=value;
+        
+            }
+            text_scal1(result,11,"1.7em");
+            text_scal1(result,12,"1.6em");
+            text_scal1(result,13,"1.5em");
+            text_scal1(result,14,"1.4em");
+            text_scal1(result,15,"1.3em");
+            text_scal1(result,16,"1.2em");
+            text_scal2(result,10,"1.8em");   
+            
+        });
+        function text_scal1(tex1 ,numberr1, em1){
+
+            if(tex1.innerText.length == numberr1){
+
+                result.style. fontSize =  em1;
+                result.style.lineHeight = "45px";
+            }
+        }
+        function text_scal2(tex1 ,numberr1, em1){
+
+            if(tex1.innerText.length < numberr1){
+
+                result.style. fontSize =  em1;
+                result.style.lineHeight = "45px";
+            }
+
+        }
+    }
+
+function wit() { alert("функция 2");}
+                
+ export{browser_version , wit};
--- index.js
+++ index.js
@@ -1,100 +1,91 @@
 
-import { alerttik ,wit } from "./chrom_calc/index.js"
-if(window.innerWidth > 500){
-alerttik();
-}
-
-if(window.innerWidth < 500){
-
-   let bloc_btn = document.querySelector(".btnContent");
-   let result = document.querySelector(".val");
-   let his = document.querySelector(".history");
-   let histroylist = document.querySelector(".histroylist");
-   let btn_history = document.querySelector(".btn_history ");
-   let h1 = document.querySelector(".h1");
-   let btn_back = document.querySelector(".btnback");
-   let display = document.querySelector(".display");
-   let value;
-   let history = "";
+import { browser_version ,wit } from "./chrom_calc/index.js";
 
 
-bloc_btn.addEventListener("click",(event)=>{
-  if(!event.target.classList.contains("btn")) return;
-      value = event.target.innerText;
-      
-      switch(value){
-         case "C":
-            result.innerText =  result.innerText.substring(0,result.innerText.length-1);
-            result.innerText = result.innerText;
-          
-            if(result.innerText == "" && !his.innerText == ""){
-               result.innerText = his.innerText;
-               his.innerText = "";
+if(window.innerWidth > 500) browser_version();
+
+
+if(window.innerWidth < 500) mobil_version();
+
+   function mobil_version(){
+
+   
+         let bloc_btn = document.querySelector(".btnContent");
+         let result = document.querySelector(".val");
+         let his = document.querySelector(".history");
+         let histroylist = document.querySelector(".histroylist");
+         let btn_history = document.querySelector(".btn_history ");
+         let h1 = document.querySelector(".h1");
+         let btn_back = document.querySelector(".btnback");
+         let display = document.querySelector(".display");
+         let value;
+         let history = "";
+
+
+      bloc_btn.addEventListener("click",(event)=>{
+      if(!event.target.classList.contains("btn")) return;
+            value = event.target.innerText;
+            
+            switch(value){
+               case "C":
+                     result.innerText =  result.innerText.substring(0,result.innerText.length-1);
+                     result.innerText = result.innerText;
+               
+                  if(result.innerText == "" && !his.innerText == ""){
+                     
+                     result.innerText = his.innerText;
+                     his.innerText = "";
+                  }
+                  break;
+               case "=":
+                     his.innerText = result.innerText ;
+                     result.innerText = eval(result.innerText);
+                     history  += `
+                        <li class="li">${his.innerText}<br>
+                              <span>${result.innerText}</span>
+                        </li>`
+                     histroylist.innerHTML  = history;  
+                     if(his.innerText.length > 12)  his.style.fontSize = "0.9em";  
+                     break;
+               default: result.innerText +=value;
+
             }
-            break;
-         case "=":
-            his.innerText = result.innerText ;
-            result.innerText = eval(result.innerText);
-            history  += `
-               <li class="li">${his.innerText}<br>
-                     <span>${result.innerText}</span>
-               </li>`
-               histroylist.innerHTML  = history;   
-               if(his.innerText.length > 12){
-                his.style.fontSize = "0.9em";
-               }   
-               break;
-         default:
-            result.innerText +=value;
+            text_scale(result,"==",11,"1.7em");
+            text_scale(result,"==",12,"1.6em");
+            text_scale(result,"==",13,"1.5em");
+            text_scale(result,"==",14,"1.4em");
+            text_scale(result,"==",15,"1.3em");
+            text_scale(result,"<",10,"1.8em");
 
-    }
-       if(result.innerText.length == 11){
-          result.style. fontSize =  "1.7em";
-          result.style.lineHeight = "101px";
-       }
-       if(result.innerText.length == 12){
-         result.style. fontSize =  "1.6em";
-         result.style.lineHeight = "101px";
-        }
-        if(result.innerText.length == 13){
-         result.style. fontSize =  "1.5em";
-         result.style.lineHeight = "101px";
-        }
-        if(result.innerText.length == 14){
-         result.style. fontSize =  "1.4em";
-         result.style.lineHeight = "101px";
-        }
-        if(result.innerText.length == 15){
-         result.style. fontSize =  "1.3em";
-         result.style.lineHeight = "101px";
-        }
-        if(result.innerText.length == 16){
-         result.style. fontSize =  "1.2em";
-         result.style.lineHeight = "101px";
-        }
-       
-        
-         if(result.innerText.length < 10){
-         display.style.color = "#fff";
-         result.style. fontSize =  "1.8em";
-        }
-     
-  });
-  btn_history.addEventListener("click",()=>{
-        bloc_btn.style.display = "none";
-        display.style.display = "none";
-        histroylist.style.display = "block";
-        h1.style.display =  "block";
-        btn_back.style.display = "block";
-     
-  });
-  btn_back.addEventListener("click",()=>{
-         bloc_btn.style.display = "block";
-         display.style.display = "block";
-         histroylist.style.display = "none";
-         h1.style.display =  "none";
-         btn_back.style.display = "none";
-  });
+            
+               
+      });
+      btn_history.addEventListener("click",()=>{
 
- 
-}
+            bloc_btn.style.display = "none";
+            display.style.display = "none";
+            histroylist.style.display = "block";
+            h1.style.display =  "block";
+            btn_back.style.display = "block";
+         
+      });
+      btn_back.addEventListener("click",()=>{
+
+            bloc_btn.style.display = "block";
+            display.style.display = "block";
+            histroylist.style.display = "none";
+            h1.style.display =  "none";
+            btn_back.style.display = "none";
+      });
+
+      function text_scale(tex , ravno ,numberr, em){
+
+         if(tex.innerText.length ,ravno, numberr){
+            
+            result.style.color = "red";
+            result.style. fontSize =  em;
+            result.style.lineHeight = "101px";
+         }
+      }
+   }
+
